ad3e804b304285d927b7161f43275f13b68d89fd
Kabiir Krishna <kabiirk@gmail.com>
2020-10-08 21:20:23 +0530

    Create naive_bayes.py

---
+++ naive_bayes.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+play_tennis = pd.read_csv("PlayTennis.csv")
+
+play_tennis
+
+number = LabelEncoder()
+play_tennis['Outlook'] = number.fit_transform(play_tennis['Outlook'])
+play_tennis['Temperature'] = number.fit_transform(play_tennis['Temperature'])
+play_tennis['Humidity'] = number.fit_transform(play_tennis['Humidity'])
+play_tennis['Wind'] = number.fit_transform(play_tennis['Wind'])
+play_tennis['Play Tennis'] = number.fit_transform(play_tennis['Play Tennis'])
+
+print(play_tennis)
