6ad80a9803d54b1a7188b6deadf730dbcca9e46e
Kabiir Krishna <kabiirk@gmail.com>
2020-10-08 21:35:41 +0530

    Create kmeans_clustering.py

---
+++ kmeans_clustering.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+'''
+#Load data_clustering.txt
+X = np.loadtxt('data_clustering.txt', delimiter=',')
+'''
+#Load data_quality.txt
+X = np.loadtxt('data_quality.txt', delimiter=',')
+num_clusters = 6
+
+# Plot input data
+plt.figure()
+plt.scatter(X[:,0], X[:,1], marker='o', facecolors='none', 
+        edgecolors='black', s=80)
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+plt.title('Input data')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+
+#create kmeans obj
+kmeans = KMeans(init='k-means++', n_clusters=num_clusters, n_init=10)
+
+#train
+kmeans.fit(X)
+
+# Step size of the mesh
+step_size = 0.01
+ 
+# Define the grid of points to plot the boundaries
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+x_vals, y_vals = np.meshgrid(np.arange(x_min, x_max, step_size), 
+        np.arange(y_min, y_max, step_size))
+
+#predict output
+output = kmeans.predict(np.c_[x_vals.ravel(), y_vals.ravel()])
+
+# Plot different regions and color them 
+output = output.reshape(x_vals.shape)
+plt.figure()
+plt.clf()
+plt.imshow(output, interpolation='nearest',
+           extent=(x_vals.min(), x_vals.max(), 
+               y_vals.min(), y_vals.max()),
+           cmap=plt.cm.Paired, 
+           aspect='auto', 
+           origin='lower')
+
+# Overlay input points
+plt.scatter(X[:,0], X[:,1], marker='o', facecolors='none', 
+        edgecolors='black', s=80)
+ 
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+plt.title('Boundaries of clusters')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
