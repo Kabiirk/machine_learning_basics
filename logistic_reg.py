26cf68fafe587623ccafef6026d7af4675ac7432
Kabiir Krishna <kabiirk@gmail.com>
2020-10-08 21:29:01 +0530

    ability to save model

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,8 +1,8 @@
 import numpy as np
-from sklearn import linear_model 
+from sklearn import linear_model
+import sklearn.metrics as sm
 import matplotlib.pyplot as plt
 import pandas as pd
-
 from utilities import visualize_classifier
 
 # Define sample input data
@@ -29,3 +29,56 @@ df1 = pd.read_csv('data_decision_trees.txt', sep=",", header=None)
 
 #UPLOAD THE FILE AGAIN !!
 df2 = pd.read_csv('data_decision_trees.txt', sep=",", header=None)
+
+import pickle
+input_file = "data_singlevar_regr.txt"
+input_file2 = "my_data1.xls"
+data = np.loadtxt(input_file, delimiter=",")
+
+data2 = pd.read_excel(input_file2, delimiter=",")
+X = data2.drop('L', axis=1)
+y = data2['L']
+data2.shape
+
+data.shape
+
+X.shape
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30)
+#create model
+regressor = linear_model.LinearRegression()
+
+#Train model
+regressor.fit(X_train, y_train)
+
+#predict
+
+y_test_pred = regressor.predict(X_test)
+
+y_test_pred
+
+#plot
+plt.scatter(X_test, y_test, color = 'green')
+plt.plot(X_test, y_test_pred, color='black', linewidth=4)
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+# Compute performance metrics
+print("Linear regressor performance:")
+print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred), 2))
+print("Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred), 2)) 
+print("Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred), 2)) 
+print("Explain variance score =", round(sm.explained_variance_score(y_test, y_test_pred), 2))
+print("R2 score =", round(sm.r2_score(y_test, y_test_pred), 2))
+
+output_model_file = 'model.pkl'
+
+#save model
+with open(output_model_file, 'wb') as f:
+    pickle.dump(regressor, f)
+
+# to read model
+with open(output_model_file, 'rb') as f:
+    regressor_model = pickle.load(f)
