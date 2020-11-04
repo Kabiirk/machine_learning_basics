e10351c0d062c16884dcd23b1ab995769990585a
Kabiir Krishna <kabiirk@gmail.com>
2020-11-04 23:20:30 +0530

    Create linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn import linear_model
+import sklearn.metrics as sm
+import matplotlib.pyplot as plt
+import pandas as pd
+
+mydata = pd.read_excel("my_data1.xls")
+
+print(mydata.head())
+
+X = mydata.drop('L', axis=1)
+y = mydata['L']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30)
+
+# Create linear regressor object
+regressor = linear_model.LinearRegression()
+
+# Train the model using the training sets
+regressor.fit(X_train, y_train)
+
+# Predict the output
+y_test_pred = regressor.predict(X_test)
+
+# Plot outputs
+plt.scatter(X_test, y_test, color='green')
+plt.plot(X_test, y_test_pred, color='orange', linewidth=4)
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+# Compute performance metrics
+print("Linear regressor performance:")
+print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred), 2))
+print("Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred), 2)) 
+print("Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred), 2)) 
+print("Explain variance score =", round(sm.explained_variance_score(y_test, y_test_pred), 2))
+print("R2 score =", round(sm.r2_score(y_test, y_test_pred), 2))
